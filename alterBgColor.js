14432e302bcc3ff10dc12b9e8d91ec126ed4736c
Xuning Tian <xtian513@gmail.com>
2015-07-04 18:59:49 +0800

    change varible name

--- alterBgColor.js
+++ alterBgColor.js
@@ -9,13 +9,14 @@
 				$("tbody>tr:odd", this).addClass(options.odd);
 				$("tbody>tr:even", this).addClass(options.even);
 				$("tbody>tr", this).click(function(){
-					var hasChecked = $(this).attr("checked");;
+					var hasChecked = $(this).attr("checked");
 					$(this)[hasChecked?"removeClass":"addClass"](options.checked)
-					.attr("checked", !hasSelected);
+					.find(":checkbox")
+					.attr("checked", !hasChecked);
 				});
 				//默认选中元素高亮显示
 				$("tbody>tr:has(:checked)").addClass(options.checked);
 				return this;
 		}
 	});
-})(jquery)
+})(jQuery)
